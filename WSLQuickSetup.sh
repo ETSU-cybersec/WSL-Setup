7f2a753e9111fe989f0a9422bf871191bd753266
bgerard578 <bgerard578@protonmail.com>
2026-07-02 21:46:37 -0400

    Add files via upload
    script to install essential ubuntu commands

---
+++ WSLQuickSetup.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Update
+apt update
+
+# Upgrade
+apt upgrade -y
+
+# Basic Installs
+apt install unzip -y
+apt install zip -y
+apt install vim -y
+apt install curl -y
+apt install git -y
+apt install wget -y
+apt install nmap -y
+apt install openssh-client -y
+apt install openssh-server -y
+apt install net-tools -y
+apt install whois -y
+apt install tcpdump -y
+apt install wireshark -y
+apt install netcat-openbsd -y
+apt install telnet -y
+apt install hydra -y
+apt install hashcat -y
+apt install sqlmap -y
+apt install gobuster -y
+apt install dirb -y
+apt install arp-scan -y
+apt install binwalk -y
+apt install exiftool -y
+apt install steghide -y
+apt install hexedit -y
+apt install gdb -y
+
+# Full Install (Will continue to be updated)
+
+
+# Python Setup
+apt install python3 -y
+apt install python3-pip -y
+apt install python3-venv -y
+apt install python3-dev -y
+apt install build-essential -y
+apt install pipx -y
+
+python3 -m venv .venv
+source .venv/bin/activate
+deactivate
+
+pipx install volatility3
+pipx install sqlmap
+pipx install ldap3
+pipx install pwntools
+
+# John Jumbo Setup
+apt install pkg-config -y
+apt install libssl-dev -y 
+apt install zlib1g-dev -y
+apt install libbz2-dev -y
+apt install libgmp-dev -y
+apt install libpcap-dev -y
+apt install libsqlite3-dev -y
+apt install libcurl4-openssl-dev -y
+apt install yasm -y
+
+git clone https://github.com/openwall/john.git
+cd john/src
+./configure
+make -sj"$(nproc)"
+
+grep -qxF 'export PATH="$HOME/tools/john/run:$PATH"' ~/.bashrc || \
+echo 'export PATH="$HOME/tools/john/run:$PATH"' >> ~/.bashrc
+
+source ~/.bashrc
+
+
+
+
